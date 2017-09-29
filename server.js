563e143d02125691b23fa5e6da9396919b331808
Travis <travis@localhost.dev>
2017-09-29 23:00:41 +1000

    Major refactor.

--- server.js
+++ server.js
@@ -1,29 +1,18 @@
 'use strict'
 
 const express = require('express')
-const Slapp = require('slapp')
-const ConvoStore = require('slapp-convo-beepboop')
-const Context = require('slapp-context-beepboop')
+const SlappHelper = require('./lib/slapp-helper')
 
-// use `PORT` env var on Beep Boop - default to 3000 locally
-var port = process.env.PORT || 3000
+// -- Arrange
 
-var slapp = Slapp({
-  // Beep Boop sets the SLACK_VERIFY_TOKEN env var
-  verify_token: process.env.SLACK_VERIFY_TOKEN,
-  convo_store: ConvoStore(),
-  context: Context()
-})
-
-
-var HELP_TEXT = `
+let port = SlappHelper.GetPort()
+let slapp = SlappHelper.CreateSlapp()
+const version = 'dolor-10'
+const HELP_TEXT = `
 I will respond to the following messages:
 \`help\` - to see this message.
-\`hi\` - to demonstrate a conversation that tracks state.
-\`thanks\` - to demonstrate a simple response.
-\`<type-any-other-text>\` - to demonstrate a random emoticon response, some of the time :wink:.
-\`attachment\` - to see a Slack attachment message.
-(beep boop!)
+\`version\` - to see version of this Slack bot.
+\`random\` - to output a random sentence, for fun.
 `
 
 
@@ -34,15 +23,16 @@ I will respond to the following messages:
 
 const COMMAND_HANDLER = '/dolor'
 
-slapp.command(COMMAND_HANDLER, 'whisper', (msg) => {
-  /**
-   * msg.response() will create response that's only visible to the requestee.
-   */
+slapp.command(COMMAND_HANDLER, 'version', (msg) => {
+  msg.say(`Version [${version}]`)
+})
+
+slapp.command(COMMAND_HANDLER, 'random', (msg) => {
   /**
    * msg.response() doesn't take array as it will just response with entire array.
    * Have to use msg.say() for random sentence usage.
    */
-  msg.respond([
+  msg.say([
     "Be happy :smile:",
     'You the best',
     ':+1: go on!',
@@ -50,52 +40,74 @@ slapp.command(COMMAND_HANDLER, 'whisper', (msg) => {
   ])
 })
 
-slapp.command(COMMAND_HANDLER, /^get/, (msg, text, match, arg4) => {
-  console.log('text:', text)
-  console.log('match:', match)
-  console.log('arg4:', arg4)
-  /**
-   * msg.say() will create normal 'bot_message' visible by everyone in the channel.
-   */
-  msg.say(`You want to get [${match}].`)
-})
-
-slapp.command(COMMAND_HANDLER, 'go (.*)', (msg, text, match, arg4) => {
-  console.log('text:', text)
-  console.log('match:', match)
-  console.log('arg4:', arg4)
-  msg.say(`You want to go [${match}].`)
+slapp.command(COMMAND_HANDLER, 'help', (msg) => {
+  msg.say(HELP_TEXT)
 })
 
 /**
  * Catch all
  */
 slapp.command(COMMAND_HANDLER, '(.*)', (msg, text, match) => {
-  console.log('text:', text)
-  console.log('match:', match)
-  msg.say(`You submitted: [${match}].`)
+  // console.log('text:', text)
+  // console.log('match:', match)
+  msg.say(`Unknown command: [${match}].`)
 })
 
 
-//
 
+//*********************************************
+// Legacy
+//*********************************************
 
-slapp.command(COMMAND_HANDLER, /^in/, (msg, text, match) => {
-  // `respond` is used for actions or commands and uses the `response_url` provided by the
-  // incoming request from Slack
-  // console.log('[dolor in]. msg:', msg)
-  // msg.respond(`Glad you are in ${match}!`)
-  msg.respond(`Glad you are in [${text}].`)
-})
+// slapp.command(COMMAND_HANDLER, 'whisper', (msg) => {
+//   /**
+//    * msg.response() will create response that's only visible to the requestee.
+//    */
+//   /**
+//    * msg.response() doesn't take array as it will just response with entire array.
+//    * Have to use msg.say() for random sentence usage.
+//    */
+//   msg.response('Dont forget umbrella.')
+// })
 
-slapp.command(COMMAND_HANDLER, 'create (.*)', (msg, text, question) => {
-  // if "/inorout create Who is in?" is received:
-  // text = create Who is in?
-  // question = Who is in?
-  // console.log('[dolor create]. msg:', msg, 'text:', text, 'question:', question)
-  // msg.response('Create something?')
-  msg.say('Create something??')
-})
+// slapp.command(COMMAND_HANDLER, /^get/, (msg, text, match, arg4) => {
+//   console.log('text:', text)
+//   console.log('match:', match)
+//   console.log('arg4:', arg4)
+//   /**
+//    * msg.say() will create normal 'bot_message' visible by everyone in the channel.
+//    */
+//   msg.say(`You want to get [${match}].`)
+// })
+
+// slapp.command(COMMAND_HANDLER, 'go (.*)', (msg, text, match, arg4) => {
+//   console.log('text:', text)
+//   console.log('match:', match)
+//   console.log('arg4:', arg4)
+//   msg.say(`You want to go [${match}].`)
+// })
+
+// // response to the user typing "help"
+// slapp.message('help', ['mention', 'direct_message'], (msg) => {
+//   msg.say(HELP_TEXT)
+// })
+
+// slapp.command(COMMAND_HANDLER, /^in/, (msg, text, match) => {
+//   // `respond` is used for actions or commands and uses the `response_url` provided by the
+//   // incoming request from Slack
+//   // console.log('[dolor in]. msg:', msg)
+//   // msg.respond(`Glad you are in ${match}!`)
+//   msg.respond(`Glad you are in [${text}].`)
+// })
+
+// slapp.command(COMMAND_HANDLER, 'create (.*)', (msg, text, question) => {
+//   // if "/inorout create Who is in?" is received:
+//   // text = create Who is in?
+//   // question = Who is in?
+//   // console.log('[dolor create]. msg:', msg, 'text:', text, 'question:', question)
+//   // msg.response('Create something?')
+//   msg.say('Create something??')
+// })
 
 // slapp.command('/dolor', 'create (.*)', (msg, text, question) => {
 //   msg.response(`You said create something: [${text}]. question: [${question}]`)
@@ -105,99 +117,94 @@ slapp.command(COMMAND_HANDLER, 'create (.*)', (msg, text, question) => {
 //   msg.response('I know you are saying something, but Im not sure what to do...')
 // })
 
+// slapp.message('foobar', ['mention', 'direct_message'], (msg) => {
+//   msg.say('FOOBAR! FOO BAR!!')
+// })
+
+// // "Conversation" flow that tracks state - kicks off when user says hi, hello or hey
+// slapp
+//   .message('^(hi|hello|hey)$', ['direct_mention', 'direct_message'], (msg, text) => {
+//     msg
+//       .say(`${text}, how are you?`)
+//       // sends next event from user to this route, passing along state
+//       .route('how-are-you', { greeting: text })
+//   })
+//   .route('how-are-you', (msg, state) => {
+//     var text = (msg.body.event && msg.body.event.text) || ''
+
+//     // user may not have typed text as their next action, ask again and re-route
+//     if (!text) {
+//       return msg
+//         .say("Whoops, I'm still waiting to hear how you're doing.")
+//         .say('How are you?')
+//         .route('how-are-you', state)
+//     }
+
+//     // add their response to state
+//     state.status = text
+
+//     msg
+//       .say(`Ok then. What's your favorite color?`)
+//       .route('color', state)
+//   })
+//   .route('color', (msg, state) => {
+//     var text = (msg.body.event && msg.body.event.text) || ''
+
+//     // user may not have typed text as their next action, ask again and re-route
+//     if (!text) {
+//       return msg
+//         .say("I'm eagerly awaiting to hear your favorite color.")
+//         .route('color', state)
+//     }
+
+//     // add their response to state
+//     state.color = text
+
+//     msg
+//       .say('Thanks for sharing.')
+//       .say(`Here's what you've told me so far: \`\`\`${JSON.stringify(state)}\`\`\``)
+//     // At this point, since we don't route anywhere, the "conversation" is over
+//   })
+
+// // Can use a regex as well
+// slapp.message(/^(thanks|thank you)/i, ['mention', 'direct_message'], (msg) => {
+//   // You can provide a list of responses, and a random one will be chosen
+//   // You can also include slack emoji in your responses
+//   msg.say([
+//     "You're welcome :smile:",
+//     'You bet',
+//     ':+1: Of course',
+//     'Anytime :sun_with_face: :full_moon_with_face:'
+//   ])
+// })
+
+// // demonstrate returning an attachment...
+// slapp.message('attachment', ['mention', 'direct_message'], (msg) => {
+//   msg.say({
+//     text: 'Check out this amazing attachment! :confetti_ball: ',
+//     attachments: [{
+//       text: 'Slapp is a robust open source library that sits on top of the Slack APIs',
+//       title: 'Slapp Library - Open Source',
+//       image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
+//       title_link: 'https://beepboophq.com/',
+//       color: '#7CD197'
+//     }]
+//   })
+// })
+
+// // Catch-all for any other responses not handled above
+// slapp.message('.*', ['direct_mention', 'direct_message'], (msg) => {
+//   // respond only 40% of the time
+//   if (Math.random() < 0.4) {
+//     msg.say([':wave:', ':pray:', ':raised_hands:'])
+//   }
+// })
+
 
 
 //*********************************************
-// Setup different handlers for messages
+// Sever Setup
 //*********************************************
-
-slapp.message('foobar', ['mention', 'direct_message'], (msg) => {
-  msg.say('FOOBAR! FOO BAR!!')
-})
-
-// response to the user typing "help"
-slapp.message('help', ['mention', 'direct_message'], (msg) => {
-  msg.say(HELP_TEXT)
-})
-
-// "Conversation" flow that tracks state - kicks off when user says hi, hello or hey
-slapp
-  .message('^(hi|hello|hey)$', ['direct_mention', 'direct_message'], (msg, text) => {
-    msg
-      .say(`${text}, how are you?`)
-      // sends next event from user to this route, passing along state
-      .route('how-are-you', { greeting: text })
-  })
-  .route('how-are-you', (msg, state) => {
-    var text = (msg.body.event && msg.body.event.text) || ''
-
-    // user may not have typed text as their next action, ask again and re-route
-    if (!text) {
-      return msg
-        .say("Whoops, I'm still waiting to hear how you're doing.")
-        .say('How are you?')
-        .route('how-are-you', state)
-    }
-
-    // add their response to state
-    state.status = text
-
-    msg
-      .say(`Ok then. What's your favorite color?`)
-      .route('color', state)
-  })
-  .route('color', (msg, state) => {
-    var text = (msg.body.event && msg.body.event.text) || ''
-
-    // user may not have typed text as their next action, ask again and re-route
-    if (!text) {
-      return msg
-        .say("I'm eagerly awaiting to hear your favorite color.")
-        .route('color', state)
-    }
-
-    // add their response to state
-    state.color = text
-
-    msg
-      .say('Thanks for sharing.')
-      .say(`Here's what you've told me so far: \`\`\`${JSON.stringify(state)}\`\`\``)
-    // At this point, since we don't route anywhere, the "conversation" is over
-  })
-
-// Can use a regex as well
-slapp.message(/^(thanks|thank you)/i, ['mention', 'direct_message'], (msg) => {
-  // You can provide a list of responses, and a random one will be chosen
-  // You can also include slack emoji in your responses
-  msg.say([
-    "You're welcome :smile:",
-    'You bet',
-    ':+1: Of course',
-    'Anytime :sun_with_face: :full_moon_with_face:'
-  ])
-})
-
-// demonstrate returning an attachment...
-slapp.message('attachment', ['mention', 'direct_message'], (msg) => {
-  msg.say({
-    text: 'Check out this amazing attachment! :confetti_ball: ',
-    attachments: [{
-      text: 'Slapp is a robust open source library that sits on top of the Slack APIs',
-      title: 'Slapp Library - Open Source',
-      image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-      title_link: 'https://beepboophq.com/',
-      color: '#7CD197'
-    }]
-  })
-})
-
-// Catch-all for any other responses not handled above
-slapp.message('.*', ['direct_mention', 'direct_message'], (msg) => {
-  // respond only 40% of the time
-  if (Math.random() < 0.4) {
-    msg.say([':wave:', ':pray:', ':raised_hands:'])
-  }
-})
 
 // attach Slapp to express server
 var server = slapp.attachToExpress(express())
@@ -208,5 +215,5 @@ server.listen(port, (err) => {
     return console.error(err)
   }
 
-  console.log(`Listening on port [${port}] (beep boop)`)
+  console.log(`Listening on port [${port}]`)
 })
